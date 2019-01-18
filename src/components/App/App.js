7760a3e1d17dcc0dd166f202b493d05c4d46f0f7
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-17 20:52:59 -0700

    Refactor App to live in components folder

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.scss';
+
+const App = () => {
+  return (
+    <div>
+      <p>
+        Me give up? No way!
+      </p>
+      <p>- Ash Ketchum</p>
+      <img src={`/public/pokemon/26.png`} />
+    </div>
+  );
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -2,17 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './style.scss'
-
-const App = () => {
-  return (
-    <div>
-      <p>
-        Me give up? No way!
-      </p>
-      <p>- Ash Ketchum</p>
-      <img src={`/public/pokemon/26.png`} />
-    </div>
-  );
-};
+import App from './components/App/App'
 
 ReactDOM.render(<App />, document.getElementById('app'));
