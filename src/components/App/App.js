b3f37750de4474f886834e553867dd2feaf9c5e3
Autumn-Martin <autumnmartin.dev@gmail.com>
2019-01-18 12:22:05 -0700

    Update App to be a class that uses PokemonIndex

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,16 +1,22 @@
-import React from 'react';
-import './App.scss';
+import {render} from 'react-dom';
+import React, {Component} from 'react';
 
-const App = () => {
-  return (
-    <div>
-      <p>
-        Me give up? No way!
-      </p>
-      <p>- Ash Ketchum</p>
-      <img src={`/public/pokemon/26.png`} />
-    </div>
-  );
-};
+import './App.scss'
+import PokemonIndex from '../PokemonIndex/PokemonIndex'
+
+class App extends Component{
+  render() {
+    return (
+      <div>
+        <p>
+          Me give up? No way!
+        </p>
+        <p>- Ash Ketchum</p>
+        <img src={`/public/pokemon/26.png`} />
+        <PokemonIndex/>
+      </div>
+    )
+  }
+}
 
 export default App;
